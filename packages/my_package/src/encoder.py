32de42039432e82e4a2819fac1cdfaf7ed0c8534
peiny51 <ningyuan@ualberta.ca>
2023-02-03 15:28:29 -0700

    fix bugs

--- packages/my_package/src/encoder.py
+++ packages/my_package/src/encoder.py
@@ -15,8 +15,8 @@ class OdometryNode(DTROS):
 
         # Initialize the DTROS parent class
         super(OdometryNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
-        #self.veh_name = rospy.get_namespace().strip("/")
-        self.veh_name = os.environ["VEHICLE_NAME"]
+        self.veh_name = rospy.get_namespace().strip("/")
+        #self.veh_name = os.environ["VEHICLE_NAME"]
         # Get static parameters
         self._radius = rospy.get_param(f'/{self.veh_name}/kinematics_node/radius', 100)
 
@@ -31,11 +31,6 @@ class OdometryNode(DTROS):
         self.left_dist = 0
         self.right_dist = 0
         
-        # Variables to calculate wheel radius every 1m
-        self.last_dist_rad_left = 0.0
-        self.last_ticks_rad_left = 0.0
-        self.last_dist_rad_right = 0.0
-        self.last_ticks_rad_right = 0.0
 
         # Subscribing to the wheel encoders
         self.sub_encoder_ticks_left = rospy.Subscriber(f'/{self.veh_name}/left_wheel_encoder_node/tick', WheelsCmdStamped, callback=self.cb_encoder_data, callback_args = 'left')
@@ -63,27 +58,27 @@ class OdometryNode(DTROS):
         """
         rospy.loginfo(f"Recieved message from {wheel} wheel\n{msg}")
         
-        ticks = msg.data
+        #ticks = msg.data
         N_total = 135
         # check if it is the first reciecved messafe and store initial encoder value
         if wheel == 'left':
             if self.initial_left_tick:
-            	self.previous_left_wheel_ticks = ticks
+            	self.previous_left_wheel_ticks = msg.data
             	self.initial_left_tick = False
             	
-            left_dist = (ticks - self.prevoius_left_wheel_ticks) * self._radius * 2 * math.pi / N_total
-            self.left_dist = self.left_dist + left_dist
-            self.previous_left_wheel_ticks = ticks
+            left_dist = (msg.data - self.prevoius_left_wheel_ticks) * self._radius * 2 * math.pi / N_total
+            self.left_dist += left_dist
+            self.previous_left_wheel_ticks = msg.data
             print("Left distance: " + self.left_dist)
 
         elif wheel == 'right':
             if self.initial_right_tick:
-            	self.previous_right_wheel_ticks = ticks
+            	self.previous_right_wheel_ticks = msg.data
             	self.initial_right_tick = False
 
-            right_dist = (ticks - self.prevoius_right_wheel_ticks) * self._radius * 2 * math.pi / N_total
-            self.right_dist = self.right_dist + self.right_dist
-            self.previous_right_wheel_ticks = ticks
+            right_dist = (msg.data - self.prevoius_right_wheel_ticks) * self._radius * 2 * math.pi / N_total
+            self.right_dist += self.right_dist
+            self.previous_right_wheel_ticks = msg.data
             print("Right distance: " + self.left_dist)
 
 
--- packages/my_package/src/test.py
+++ packages/my_package/src/test.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+print("python running ...")
 import numpy as np
 import os
 import rospy
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
 from duckietown_msgs.msg import Twist2DStamped, WheelEncoderStamped, WheelsCmdStamped
 from std_msgs.msg import Header, Float32
+from std_srvs.srv import Trigger, TriggerResponse
 
 class OdometryNode(DTROS):
 
@@ -16,44 +18,109 @@ class OdometryNode(DTROS):
         # Initialize the DTROS parent class
         super(OdometryNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
         self.veh_name = rospy.get_namespace().strip("/")
-                
-        # Get static parameters
+        rospy.loginfo(f"Using vehicle name {self.veh_name}")
+
+        # Get/set static parameters
         self._radius = rospy.get_param(f'/{self.veh_name}/kinematics_node/radius', 100)
+        self.N_total = 135
+        self.initialised_ticks_left = False
+        self.initialised_ticks_right = False
+        self.wheel_distance_left = 0
+        self.wheel_distance_right = 0
 
-        # Subscribing to the wheel encoders
-        self.sub_encoder_ticks_left = rospy.Subscriber(f'/{self.veh_name}/left_wheel_encoder_node/tick', WheelEncoderStamped, self.cb_encoder_data_left, queue_size=1)
-        self.sub_encoder_ticks_right = rospy.Subscriber(f'/{self.veh_name}/right_wheel_encoder_node/tick', WheelEncoderStamped, self.cb_encoder_data_right, queue_size=1)
-        self.sub_executed_commands = rospy.Subscriber(f'/{self.veh_name}/wheels_driver_node/wheels_cmd_executed', WheelsCmdStamped, self.cb_executed_commands, queue_size=1)
+        self.calibration_distance = 1.0
+        
 
-        # Publishers
-        self.pub_integrated_distance_left = rospy.Publisher(f'/{self.veh_name}/my_odometry_node/encoder_data_left', Float32, queue_size=1)
-        self.pub_integrated_distance_right = rospy.Publisher(f'/{self.veh_name}/my_odometry_node/encoder_data_right', Float32, queue_size=1)
+        # Subscribing to the wheel encoders (executed commands not needed since
+        # negative ticks update)
+        self.sub_encoder_ticks_left = rospy.Subscriber(
+            f'/{self.veh_name}/left_wheel_encoder_node/tick',\
+            WheelEncoderStamped, self.cb_encoder_data, callback_args="left"
+            )
+        self.sub_encoder_ticks_right = rospy.Subscriber(
+            f'/{self.veh_name}/right_wheel_encoder_node/tick',\
+            WheelEncoderStamped, self.cb_encoder_data, callback_args="right"
+            )
+        # self.sub_executed_commands = rospy.Subscriber(f'/{self.veh_name}/wheels_driver_node/wheels_cmd_executed', WheelsCmdStamped, self.cb_executed_commands)
+
+        # Publish the calculated wheel displacement distances
+        self.pub_wheel_distance_left = rospy.Publisher(f'/{self.veh_name}/wheel_distance_left', Float32, queue_size=10)
+        self.pub_wheel_distance_right = rospy.Publisher(f'/{self.veh_name}/wheel_distance_right', Float32, queue_size=10)
+
+        # Service to find wheel radius
+        #self.serv_wheel_calibrate = rospy.Service(f'/{self.veh_name}/wheel_calibration', Trigger, self.calibrate)
 
         self.log("Initialized")
 
-    def cb_encoder_data_left(self, msg):
+
+    def calibrate(self, data):
+        """ Find the wheel radius based on the calibration distance.
+        """
+
+        self.wheel_radius_left = self.N_total / (self.total_ticks_left-self.initial_ticks_left) * self.calibration_distance / (2.0 * np.pi)
+        self.wheel_radius_right = self.N_total / (self.total_ticks_right-self.initial_ticks_right) * self.calibration_distance / (2.0 * np.pi)
+        self.wheel_radius_average = (self.wheel_radius_left + self.wheel_radius_right) / 2
+
+        #rospy.loginfo(f'Left wheel radius: {self.wheel_radius_left}')
+        #rospy.loginfo(f'Right wheel radius: {self.wheel_radius_right}')
+        #rospy.loginfo(f'Wheel radius average was calculated as {self.wheel_radius_average}m, assuming the calibration distance was {self.calibration_distance}m.')
+
+        return TriggerResponse(
+            success = True,
+            message = f'For a calibration distance of {self.calibration_distance}: The left wheel radius is {self.wheel_radius_left}, the right wheel radius is {self.wheel_radius_right} the average wheel radius is {self.wheel_radius_average}m'
+        )
+        
+
+    def cb_encoder_data(self, msg, wheel):
         """ Update encoder distance information from ticks.
         """
-        data = msg.data
-        res = msg.resolution
-        dist = 2*3.14159*self._radius*data/res
 
-        self.pub_integrated_distance_left.publish(dist)
+        rospy.loginfo(f"Recieved message from {wheel} wheel\n{msg}")
+
+        if (wheel == "left"):
+            # use flag for if ticks have not been recorded yet
+            if (not self.initialised_ticks_left):
+                self.total_ticks_left = msg.data
+                self.initial_ticks_left = msg.data
+                self.initialised_ticks_left = True
+
+            self.dN_ticks_left = msg.data - self.total_ticks_left
+            self.wheel_distance_left += 2 * np.pi * self._radius \
+                * self.dN_ticks_left / self.N_total
+
+            self.total_ticks_left = msg.data
+
+        elif (wheel == "right"):
+            # use flag for if ticks have not been recorded yet
+            if (not self.initialised_ticks_right):
+                self.total_ticks_right = msg.data
+                self.initial_ticks_right = msg.data
+                self.initialised_ticks_right = True
+
+            self.dN_ticks_right = msg.data - self.total_ticks_right
+            self.wheel_distance_right += 2 * np.pi * self._radius \
+                * self.dN_ticks_right / self.N_total
+
+            self.total_ticks_right = msg.data
+
+        else:
+            raise NameError("wheel name not found")
 
 
-    def cb_encoder_data_right(self, msg):
-        """ Update encoder distance information from ticks.
-        """
-        data = msg.data
-        res = msg.resolution
-        dist = 2*3.14159*self._radius*data/res
+    def run(self):
+        rate = rospy.Rate(10)
 
-        self.pub_integrated_distance_right.publish(dist)
+        while not rospy.is_shutdown():
+            self.pub_wheel_distance_left.publish(self.wheel_distance_left)
+            self.pub_wheel_distance_right.publish(self.wheel_distance_right)
+            rospy.loginfo("Left distance: %f" %self.wheel_distance_left)
+            rospy.loginfo("Right distance: %f" %self.wheel_distance_right)
 
-
+            rate.sleep()
 
 if __name__ == '__main__':
     node = OdometryNode(node_name='my_odometry_node')
+    rospy.loginfo("wheel_encoder_node is up and running...")
+    node.run()
     # Keep it spinning to keep the node alive
     rospy.spin()
-    rospy.loginfo("wheel_encoder_node is up and running...")
